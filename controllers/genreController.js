9fc22617e495346c331d1c71c0afff0dcba7bdf3
DavidxDl <Davidbekamlopez@gmail.com>
2024-01-09 05:13:05 -0300

    initial commit

---
+++ controllers/genreController.js
@@ -0,0 +1,87 @@
+const Genre = require("../models/genre");
+const Book = require("../models/book");
+const {body, validationResult} = require("express-validator")
+const asyncHandler = require("express-async-handler");
+
+// Display list of all Genre.
+exports.genre_list = asyncHandler(async (req, res, next) => {
+  const allGenres = await Genre.find().sort({ name: 1 }).exec();
+  res.render("genre_list", { title: "Genre list", genre_list: allGenres });
+});
+
+// Display detail page for a specific Genre.
+exports.genre_detail = asyncHandler(async (req, res, next) => {
+  const [genre, booksInGenre] = await Promise.all([
+    Genre.findOne({ _id: req.params.id }).exec(),
+    Book.find({ genre: req.params.id }).exec(),
+  ]);
+  if (genre === null) {
+    const err = new Error("Genre not found");
+    res.status = 404;
+    next(err);
+  }
+  res.render("genre_detail", {
+    title: "Genre Detail",
+    genre: genre.name,
+    genre_books: booksInGenre,
+  });
+});
+
+// Display Genre create form on GET.
+exports.genre_create_get = (req, res, next) => {
+  res.render("genre_form", {title: 'Genre Form'});
+};
+
+// Handle Genre create on POST.
+exports.genre_create_post =  [
+    body("genre_name", "name shoud at least have 3 characters")
+    .trim()
+    .isLength({min: 3})
+    .escape(),
+asyncHandler(async (req, res, next) => {
+    
+    const errors = validationResult(req);
+    const genre = new Genre({name: req.body.genre_name});
+
+    if(!errors.isEmpty()) {
+        res.render("genre_form", {
+            title: "Genre Form",
+            genre: genre,
+            errors: errors.array()
+        })
+        return;
+    } else {
+        const genreExist = await Genre
+            .findOne({name: req.body.genre_name})
+            .exec();
+        if(genreExist) {
+            res.redirect(genreExist.url)
+        } else {
+            await genre.save();
+            res.redirect(genre.url);
+        }
+    }
+}
+)
+
+]
+
+// Display Genre delete form on GET.
+exports.genre_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Genre delete GET");
+});
+
+// Handle Genre delete on POST.
+exports.genre_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Genre delete POST");
+});
+
+// Display Genre update form on GET.
+exports.genre_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Genre update GET");
+});
+
+// Handle Genre update on POST.
+exports.genre_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Genre update POST");
+});
