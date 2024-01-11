5bdd709e0da131298349931db4bf6e65d53e0f74
DavidxDl <Davidbekamlopez@gmail.com>
2024-01-11 05:06:13 -0300

    now you can update authors

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -120,10 +120,62 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Author update form on GET.
 exports.author_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
+  const author = await Author.findById(req.params.id).exec();
+  if (author === null) res.redirect("/catalog/authors");
+
+  res.render("author_form", {
+    title: "Update Author",
+    author: author,
+  });
 });
 
 // Handle Author update on POST.
-exports.author_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
-});
+exports.author_update_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage("First name must have at least 3 characters")
+    .isAlphanumeric()
+    .withMessage("First name shouldn't contain numbers!"),
+
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must have at least 3 characters")
+    .isAlphanumeric()
+    .withMessage("Family name have non-alphaNumeric Caracters"),
+
+  body("birth_date", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  body("birth_date", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.birth_date,
+      date_of_death: req.body.death_date,
+      _id: req.params.id,
+    });
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Author Form",
+        author: author,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await Author.findByIdAndUpdate(req.params.id, author, {});
+      res.redirect(author.url);
+    }
+  }),
+];
